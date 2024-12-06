0e285a57771e587a6f633428baec5782745759f2
unknown <nlastya1994@mai.ru>
2024-12-06 10:01:20 +0300

    edit

---
+++ travelai/src/components/VerticalCard/VerticalCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import style from 'card.modules.css'
+
+
+const VerticalCard = (props) => {
+
+    return (<div className={style.card}>
+        <div className={style.bothInfoDate}>
+            <div className={style.listInfo}>
+                <h3>{props?.name ? props?.name : 'Сибирь Тур№1'}</h3>
+                <p className={style.location}>
+                    <imf src="./like.svg"/>
+                </p>
+
+                <div className={style.infoRow}></div>
+
+            </div>
+            <div className={style.buttons}>
+                <button className={'mint-btn'}>Посмотреть тур</button>
+                <button className={'btn-like'}>
+                    <img src="./icons/like.svg"/>
+                </button>
+            </div>
+        </div>
+    </div>)
+
+
+}
+
+export default VerticalCard;
