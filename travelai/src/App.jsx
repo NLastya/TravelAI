54a55a55a03e8125d0bff47d3b35643d698696f1
NLastya <nlastya1994@mail.ru>
2024-12-21 11:34:33 +0300

    dg

--- travelai/src/App.jsx
+++ travelai/src/App.jsx
@@ -1,35 +1,29 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
 import SearchPage from './pages/searchPage';
-// import {Routes, Route} from 'react-router'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PopularTours from './pages/popularTours';
 import PersonalPage from './pages/personalPage';
 import TourPage from './pages/TourPage';
-import { useState } from 'react';
-import {AuthProvider, useAuth} from './hooks/useAuth';
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider, useAuth } from './hooks/useAuth';
 
 function App() {
   const { isLogged, login, logout } = useAuth();
   const [listTours, setListTour] = useState([]);
 
   return (
-    <>
-    <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path='/tours' element={<SearchPage listTours={listTours} setListTour={setListTour}/>}/>
-        <Route path='/popularTours' element={<PopularTours/>}/>
-        <Route path='/user/:user_id' element={<PersonalPage/>}/>
-        <Route path='/tour/:tour_id' element={<TourPage/>}/>
-      </Routes>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path='/tours' element={<SearchPage listTours={listTours} setListTour={setListTour} />} />
+          <Route path='/popularTours' element={<PopularTours />} />
+          <Route path='/user/:user_id' element={<PersonalPage />} />
+          <Route path='/tour/:tour_id' element={<TourPage />} />
+        </Routes>
       </AuthProvider>
-    </BrowserRouter>
-    </>
-
-    
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
