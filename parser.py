e7dcddb27be5715ac34c9a4fd2385cac3f6fd327
Olchik <krepostnaya.o@gmail.com>
2024-12-21 11:10:29 +0300

    Update parser.py
    meow((

--- parser.py
+++ parser.py
@@ -6,10 +6,7 @@ import requests
 import re
 import time
 
-options = ChromeOptions()
-options.page_load_strategy = 'eager'
 
-driver = webdriver.Chrome(options=options)
 
 def extract_city_and_id(url):
     pattern = r"weather-([a-zA-Z\-]+)-(\d+)/month/"
@@ -31,32 +28,40 @@ def get_city_id(request):
     city, city_id = extract_city_and_id(first_link)
     return city, city_id
 
-request = "магнитогорск"
-request = request + " погода гисметео на месяц"
-city, city_id = get_city_id(request)
-site = f"https://www.gismeteo.ru/weather-{city}-{city_id}/month/"
-driver.get(site)
-
-soap = bs4.BeautifulSoup(driver.page_source, "html.parser")
-
-DATA = soap.find_all("a", class_ = "row-item row-item-month-date")
-
 def get_mounth(date):
-    if "дек" in date:
-        return 12
-    if "янв" in date:
-        return 1
+    if " дек" in date:
+        return "12"
+    if " янв" in date:
+        return "01"
     else:
         return date[-1]
 
-for el in DATA:
-    date = el.find("div", class_ = "date").get_text(strip = True)
-    if len(date)>2:
-        meow = get_mounth(date)
-        date = date.replace("дек", "")
-        date = date.replace("янв", "")
-    print(f"{int(date)}.{meow}", end =" ")
-    maxt = el.find("div", class_ = "maxt").get_text(strip = True)
-    mint = el.find("div", class_ = "mint").get_text(strip = True)
-    print(f"Макс: {maxt} Мин: {mint}",end = " ")
-    print(el.get("data-tooltip"))
+def get_weather(data_start, data_end, city_request):
+    weather_data=[]
+    options = ChromeOptions()
+    options.page_load_strategy = 'eager'
+    driver = webdriver.Chrome(options=options)
+    soap = bs4.BeautifulSoup(driver.page_source, "html.parser")
+    DATA = soap.find_all("a", class_ = "row-item row-item-month-date")
+    city_request = city_request + " погода гисметео на месяц"
+    city, city_id = get_city_id(city_request)
+
+    site = f"https://www.gismeteo.ru/weather-{city}-{city_id}/month/"
+    driver.get(site)
+
+
+    for el in DATA:
+        date = el.find("div", class_ = "date").get_text(strip = True)
+        if len(date)>2:
+            meow = get_mounth(date)
+            date = date.replace(" дек", "")
+            date = date.replace(" янв", "")
+        if len(date)==1:
+                date = "0"+ date
+        date = date + "." + meow
+
+        if (date >= data_start) or (date <= data_end):
+            what_weather = el.get("data-tooltip")
+            weather_data.append([date, what_weather])
+    return weather_data
+print(get_weather("27.12", "08.01", "магнитогорск"))
